ecd88387411c573483788a5f5dd382f58cf772e7
Mrtasin <mohammadtasinspj@gmail.com>
2026-02-14 10:19:04 +0530

    refactor mergeTwoLists method for improved readability and consistency

--- MergeTwoSortedLists.py
+++ MergeTwoSortedLists.py
@@ -3,18 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from typing import Optional
+
 
 
 class Solution:
-    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         s = e = None
         while l1 and l2:
             if l1.val < l2.val:
                 temp = l1
                 l1 = l1.next
                 temp.next = None
-                if(s == None):
+                if s == None:
                     s = e = temp
                 else:
                     e.next = temp
@@ -23,13 +25,19 @@ class Solution:
                 temp = l2
                 l2 = l2.next
                 temp.next = None
-                if(s == None):
+                if s == None:
                     s = e = temp
                 else:
                     e.next = temp
                     e = temp
         if l1:
-            e.next = l1       
+            if s == None:
+                s = l1
+            else:
+                e.next = l1
         elif l2:
-            e.next = l2
+            if s == None:
+                s = l2
+            else:
+                e.next = l2
         return s
